a38d614d114e2892b9cbb2470a42631a187dbb83
iTrishank <trishank9999@gmail.com>
2025-12-19 20:47:22 +0300

    Fix quote API body parsing and validation

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ const httpServer = createServer(app);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://indianleto.com");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept");
 
   if (req.method === "OPTIONS") {
     return res.sendStatus(204);
